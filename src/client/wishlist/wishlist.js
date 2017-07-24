96095aa1f090cd56a0a02ec311d1fcbb7424d84a
gnzandrs <gavr@outlook.cl>
2017-07-24 14:23:12 -0400

    some js refactor

--- src/client/wishlist/wishlist.js
+++ src/client/wishlist/wishlist.js
@@ -6,7 +6,8 @@ let foundation = require('foundation-sites')
 let Dropzone = require('dropzone')
 let GMaps = require('gmaps')
 let { searchLocation, getMarkers, imageDelete }
-  = require('../wish-api-client')
+= require('../wish-api-client')
+
 
 page('/wishlist/create', create)
 
@@ -28,19 +29,14 @@ function create () {
       acceptedFiles: ".png, .jpg, .jpeg, .gif",
       //createImageThumbnails: true,
       addRemoveLinks: false,
-      /*removedfile: function (file) {
-
-          var _ref;
-          return (_ref = file.previewElement) != null ? ref.parentNode.removeChild(file.previewElement) : void 0;
-      },*/
-      dictDefaultMessage: "Drop File(s) Here or Click to Upload",
+      dictDefaultMessage: "Arrastrar imagenes para cargar.",
 
       init: function () {
           this.on("addedfile", function (file) {
-              var _this = this
+              let _this = this
 
               // boton quitar
-              var removeButton = Dropzone.createElement("<a data-id='"+ file.name +"' class='tiny button'>Quitar</a>")
+              let removeButton = Dropzone.createElement("<a data-id='"+ file.name +"' class='tiny button'>Quitar</a>")
 
               // Listen to the click event
               removeButton.addEventListener("click", function (e) {
@@ -50,8 +46,6 @@ function create () {
 
                   // Remove the file preview.
                   _this.removeFile(file)
-                  // If you want to the delete the file on the server as well,
-                  // you can do the AJAX request here.
               })
 
               // Add the button to the file preview element.
@@ -59,10 +53,13 @@ function create () {
 
               /* evento borrar imagen cargada
               (aca deberia ir con un handler de <a> pero no funciona con el modal) */
-              var $formDropzone = $('#myModal').find('.dz-preview').find('.tiny')
+              let $formDropzone = $('#myModal')
+                .find('.dz-preview')
+                .find('.tiny')
+
               $formDropzone.on('click', function(e) {
-                  var $this = $(this)
-                  var nombre = $this.data('id')
+                  let $this = $(this)
+                  let nombre = $this.data('id')
 
                   imageDelete (function (response) {
                     console.log('image deleted.')
@@ -78,7 +75,7 @@ function create () {
     let lat, lng
 
     // santiago location
-    var map = new GMaps({
+    let map = new GMaps({
         div: '#map-container',
         lat: -33.4691199,
         lng: -70.641997
@@ -107,13 +104,12 @@ function create () {
 
     //cargar marcadores BD
     getMarkers (function (response) {
-      if (respuesta) {
-          for (var i = 0; i < respuesta.length; i++)
-          {
+      if (response) {
+          for (let i in response.length) {
               map.addMarker({
-                  lat: respuesta[i].latitude,
-                  lng: respuesta[i].longitude,
-                  infoWindow: { content: '<div id="ver"><br><center><label>' + respuesta[i].name + '</label><center><br></div>' }
+                  lat: response[i].latitude,
+                  lng: response[i].longitude,
+                  infoWindow: { content: '<div id="ver"><br><center><label>' + response[i].name + '</label><center><br></div>' }
               })
           }
       } else {
